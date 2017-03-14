95adccc0895beb002b25d1a8c3f231ff311efac7
ericmaxwell2003 <ericmaxwell2003@yahoo.com>
2017-03-14 14:24:48 -0400

    Update Admin VC

--- macOS/SurveyAdmin/ViewController.swift
+++ macOS/SurveyAdmin/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  PopBoard
-//
-//  Created by Marin Todorov on 2/7/17.
-//  Copyright © 2017 Realm Inc. All rights reserved.
-//
-
 import Cocoa
 import RealmSwift
 
