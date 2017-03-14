83cf3360d39dc806016331840c17954dd98ed44c
ericmaxwell2003 <ericmaxwell2003@yahoo.com>
2017-03-14 15:54:12 -0400

    Add demo-script md and removed the code from the base project

--- iOS/RealmSurveys/RealmConnect.swift
+++ iOS/RealmSurveys/RealmConnect.swift
@@ -9,26 +9,7 @@ let user = "survey@demo.io"
 let pass = "password"
 
 func connect(completion: @escaping () -> Void) {
-
+    
     DispatchQueue.main.async(execute: completion)
 
-//    let cred = SyncCredentials.usernamePassword(
-//        username: user, password: pass, register: false)
-//    
-//    SyncUser.logIn(with: cred, server: serverURL) { user, error in
-//
-//        guard let user = user else {
-//            return DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-//                connect(completion: completion)
-//            }
-//        }
-//        
-//        var config = Realm.Configuration.defaultConfiguration
-//        config.schemaVersion = 1
-//        config.syncConfiguration = SyncConfiguration(user: user, realmURL: syncURL)
-//        Realm.Configuration.defaultConfiguration = config
-//        
-//        DispatchQueue.main.async(execute: completion)
-//    }
-    
 }
