3860b230244b38cb4d50c0d5f40614a31b5b7e0f
ericmaxwell2003 <ericmaxwell2003@yahoo.com>
2017-03-17 16:11:18 -0400

    Updates for open sourcing

--- Android/app/src/main/java/realm/io/realmsurveys/controller/SurveyActivity.java
+++ Android/app/src/main/java/realm/io/realmsurveys/controller/SurveyActivity.java
@@ -1,17 +1,31 @@
+/*
+ * Copyright 2017 Realm Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package realm.io.realmsurveys.controller;
 
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import java.util.UUID;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import io.realm.ObjectServerError;
 import io.realm.Realm;
 import io.realm.RealmResults;
@@ -25,22 +39,23 @@ import realm.io.realmsurveys.model.Question;
 
 public class SurveyActivity extends AppCompatActivity  {
 
+    public static final String TAG = SurveyActivity.class.getName();
+
     public static final String REALM_URL = "realm://" + BuildConfig.OBJECT_SERVER_IP + ":9080/~/survey";
     public static final String AUTH_URL = "http://" + BuildConfig.OBJECT_SERVER_IP + ":9080/auth";
     public static final String ID = "survey@demo.io";
     public static final String PASSWORD = "password";
 
-    @BindView(R.id.questionsList) public RecyclerView recyclerView;
+    private RecyclerView recyclerView;
     private SharedPreferences sharedPreferences;
-
     private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
-        ButterKnife.bind(this);
-        initRealm();
+        recyclerView = (RecyclerView) findViewById(R.id.questionsList);
+        login();
     }
 
     @Override
@@ -53,32 +68,34 @@ public class SurveyActivity extends AppCompatActivity  {
         }
     }
 
-    private void initRealm() {
+    private void login() {
 
-        SyncUser syncUser = SyncUser.currentUser();
-        if(syncUser != null)  {
-            syncUser.logout();
+        SyncUser user = SyncUser.currentUser();
+        if(user != null) {
+            postLogin(user);
+
+        } else {
+
+            final SyncCredentials syncCredentials = SyncCredentials.usernamePassword(ID, PASSWORD, false);
+
+            SyncUser.loginAsync(syncCredentials, AUTH_URL, new SyncUser.Callback() {
+                @Override
+                public void onSuccess(SyncUser user) { postLogin(user); }
+
+                @Override
+                public void onError(ObjectServerError error) {
+                    logError(error);
+                }
+            });
+
         }
-
-        final SyncCredentials syncCredentials = SyncCredentials.usernamePassword(ID, PASSWORD, false);
-        SyncUser.loginAsync(syncCredentials, AUTH_URL, new SyncUser.Callback() {
-            @Override
-            public void onSuccess(SyncUser user) {
-                SyncConfiguration syncConfiguration = new SyncConfiguration.Builder(user, REALM_URL).build();
-                Realm.setDefaultConfiguration(syncConfiguration);
-                realm = Realm.getDefaultInstance();
-                postLogin();
-            }
-
-            @Override
-            public void onError(ObjectServerError error) {
-                logError(error);
-            }
-        });
-
     }
 
-    private void postLogin() {
+    private void postLogin(SyncUser user) {
+
+        setRealmDefaultConfig(user);
+
+        realm = Realm.getDefaultInstance();
 
         RealmResults<Question> questions = realm
                 .where(Question.class)
@@ -94,8 +111,14 @@ public class SurveyActivity extends AppCompatActivity  {
 
     }
 
+    private void setRealmDefaultConfig(SyncUser user) {
+        Log.d(TAG, "Connecting to Sync Server at : ["  + REALM_URL.replaceAll("~", user.getIdentity()) + "]");
+        final SyncConfiguration syncConfiguration = new SyncConfiguration.Builder(user, REALM_URL).schemaVersion(1).build();
+        Realm.setDefaultConfiguration(syncConfiguration);
+    }
+
     private void logError(Throwable e) {
-        Log.e("SurveyActivity", e.getMessage());
+        Log.e(TAG, "Realm Error", e);
     }
 
     public void onQuestionAnswered(final String questionId, final boolean response) {
@@ -123,7 +146,8 @@ public class SurveyActivity extends AppCompatActivity  {
                     }
                 });
             }
-        }, 200);
+        }, 200); // delayed for 200ms to show the user their selection for 200ms
+                 // before it is answered and disappears from the list.
     }
 
     private String uniqueUserId() {
