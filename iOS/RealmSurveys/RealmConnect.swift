8731125bb843559181129de56416146f9155d41d
ericmaxwell2003 <ericmaxwell2003@yahoo.com>
2017-03-17 15:29:20 -0400

    Add liscense headers to files.

--- iOS/RealmSurveys/RealmConnect.swift
+++ iOS/RealmSurveys/RealmConnect.swift
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2017 Realm Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 import Foundation
 import RealmSwift
 
-let host = "45.55.167.56"
+let host = "" // <-- Enter your Realm Object Server IP here
 let serverURL = URL(string: "http://\(host):9080")!
 let syncURL = URL(string: "realm://\(host):9080/~/survey")!
 
